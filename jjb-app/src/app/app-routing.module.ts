2cd79fd40090f83d78b4b6d321ea95a5567ceb68
Justin Buist <justin.buist@gmail.com>
2023-05-08 10:41:21 -0400

    Oh, basic stuff

--- jjb-app/src/app/app-routing.module.ts
+++ jjb-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from 'src/app/components/pages/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
